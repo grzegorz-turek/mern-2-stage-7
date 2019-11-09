53c7546ce39f32602b8d521dec35cdad9466274a
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-09 12:22:34 +0100

    Fixed client config.js v.2

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 //export const API_URL = 'http://localhost:8000/api';
 //export const BASE_URL = 'http://localhost:3000';
 
-export const API_URL = '/api';
-export const BASE_URL = 'https://kodilla-blog.herokuapp.com'
+export const API_URL = 'https://kodilla-blog.herokuapp.com/api';
+//export const BASE_URL = 'https://kodilla-blog.herokuapp.com'
