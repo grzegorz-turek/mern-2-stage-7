32039c451e26fb3fd92f38e373e0d8bba3679be4
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-09 11:11:35 +0100

    Config vars moved to heroku

--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    PORT: 8000,
+    PORT: process.env.PORT,
+    DB: process.env.DB
+    //PORT: 8000,
     //DB: 'mongodb://localhost:27017/mernapp',
-    DB: 'mongodb+srv://rhino-gt:sNtWTKYeDw8MOICE@cluster0-fvdhj.mongodb.net/test?retryWrites=true&w=majority'
+    //DB: 'mongodb+srv://rhino-gt:sNtWTKYeDw8MOICE@cluster0-fvdhj.mongodb.net/test?retryWrites=true&w=majority'
 };
