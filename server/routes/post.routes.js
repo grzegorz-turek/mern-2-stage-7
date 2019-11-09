6a64f626610c0be2e1ac6d7ab13ff00872b31c58
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-09 19:24:34 +0100

    Fix heroku serving v.n+1

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -6,13 +6,13 @@ const PostController = require('../controllers/post.controller');
 // get all posts
 router.route('/posts').get(PostController.getPosts);
 
+// get posts by range
+router.route('/posts/range/:startAt/:limit').get(PostController.getPostsByRange);
+
 // get post by its ID
 router.route('/posts/:id').get(PostController.getSinglePost);
 
 // add new post
 router.route('/posts').post(PostController.addPost);
-
-// get posts by range
-router.route('/posts/range/:startAt/:limit').get(PostController.getPostsByRange);
 
 module.exports = router;
