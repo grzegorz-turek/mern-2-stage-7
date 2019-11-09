0395b72d9142031fe5b5870ed21a4fd58ab509be
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-09 15:07:48 +0100

    Fixed postsRedux.js for heroku serving v.2

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { API_URL } from '../config';
+import { API_URL, BASE_URL } from '../config';
 
 /* SELECTORS */
 export const getPosts = ({ posts }) => posts.data;
