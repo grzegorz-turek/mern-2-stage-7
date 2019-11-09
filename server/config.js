5145af1eab7448c985766978ba33e9a81e619b18
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-09 22:06:44 +0100

    Final heroku serving bugfix

--- server/config.js
+++ server/config.js
@@ -2,11 +2,11 @@ module.exports = {
     /* vars hidden on mongo server */
     PORT: process.env.PORT || 8000,
 
-    /* vars not hidden */
-    //PORT: 8000,
-
     /* remote db, vars hidden on mongo server */
     DB: process.env.DB
+
+    /* vars not hidden */
+    //PORT: 8000,
 
     /* remote db, vars unhidden */
     //DB: 'mongodb+srv://rhino-gt:sNtWTKYeDw8MOICE@cluster0-fvdhj.mongodb.net/test?retryWrites=true&w=majority'
@@ -14,13 +14,3 @@ module.exports = {
     /* local db */
     //DB: 'mongodb://localhost:27017/mernapp'
 };
-
-/*
-const dotenv = require('dotenv');
-dotenv.config();
-
-module.exports = {
-    PORT: process.env.PORT,
-    DB: process.env.DB
-};
-*/
