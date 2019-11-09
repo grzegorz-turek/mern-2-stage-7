34d798f8d771efc6710add0396a16d976902b070
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-09 21:11:38 +0100

    Fix heroku serving v.n+3

--- server/config.js
+++ server/config.js
@@ -1,15 +1,15 @@
 module.exports = {
     /* vars hidden on mongo server */
-    //PORT: process.env.PORT,
+    PORT: process.env.PORT || 8000,
 
     /* vars not hidden */
-    PORT: 8000,
+    //PORT: 8000,
 
     /* remote db, vars hidden on mongo server */
-    //DB: process.env.DB
+    DB: process.env.DB
 
     /* remote db, vars unhidden */
-    DB: 'mongodb+srv://rhino-gt:sNtWTKYeDw8MOICE@cluster0-fvdhj.mongodb.net/test?retryWrites=true&w=majority'
+    //DB: 'mongodb+srv://rhino-gt:sNtWTKYeDw8MOICE@cluster0-fvdhj.mongodb.net/test?retryWrites=true&w=majority'
 
     /* local db */
     //DB: 'mongodb://localhost:27017/mernapp'
