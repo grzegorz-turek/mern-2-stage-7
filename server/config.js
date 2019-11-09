d2f827c054323993e65cca8399aeaebd482aed8e
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-09 19:27:04 +0100

    Fix heroku serving v.n+2

--- server/config.js
+++ server/config.js
@@ -9,7 +9,7 @@ module.exports = {
     //DB: process.env.DB
 
     /* remote db, vars unhidden */
-    DB: 'mongodb+srv://rhino-gt:<sNtWTKYeDw8MOICE>@cluster0-fvdhj.mongodb.net/test?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://rhino-gt:sNtWTKYeDw8MOICE@cluster0-fvdhj.mongodb.net/test?retryWrites=true&w=majority'
 
     /* local db */
     //DB: 'mongodb://localhost:27017/mernapp'
