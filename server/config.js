33c0d05c2a08f1263650c53a5ee00cc79ddd2134
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-09 14:11:45 +0100

    Fixed client config.js v.5

--- server/config.js
+++ server/config.js
@@ -1,7 +1,16 @@
 module.exports = {
+    /* vars hidden on mongo server */
     //PORT: process.env.PORT,
-    //DB: process.env.DB
+
+    /* vars not hidden */
     PORT: 8000,
+
+    /* remote db, vars hidden on mongo server */
+    //DB: process.env.DB
+
+    /* remote db, vars unhidden */
     DB: 'mongodb+srv://rhino-gt:sNtWTKYeDw8MOICE@cluster0-fvdhj.mongodb.net/test?retryWrites=true&w=majority'
+
+    /* local db */
     //DB: 'mongodb://localhost:27017/mernapp'
 };
